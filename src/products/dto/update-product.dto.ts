23bc4fb3cd7f55b685341c4c3e4342502267ec12
Agnes Anosike <agnesanosike@gmail.com>
2026-02-25 23:18:38 +0100

    feat: service logic and controller endpoints created

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
+import { IsNumber, IsString } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+  @IsString()
+  name?: string;
+
+  @IsString()
+  description?: string;
+
+  @IsNumber()
+  price?: number;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
+type Product = CreateProductDto & {
+  id: number;
+  createdAt: Date;
+  updatedAt?: Date;
+};
+
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private products: Product[] = [];
+
+  create(createProductDto: CreateProductDto): Product {
+    const product: Product = {
+      id: this.products.length + 1,
+      ...createProductDto,
+      createdAt: new Date(),
+    };
+    this.products.push(product);
+    return product;
   }
 
-  findAll() {
-    return `This action returns all products`;
+  findAll(): Product[] {
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: number): Product {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: number, updateProductDto: UpdateProductDto): Product {
+    const productIndex = this.products.findIndex((p) => p.id === id);
+    if (productIndex === -1) {
+      throw new NotFoundException(`Product ${id} not found`);
+    }
+
+    const updatedProduct: Product = {
+      ...this.products[productIndex],
+      ...updateProductDto,
+      updatedAt: new Date(),
+    };
+
+    this.products[productIndex] = updatedProduct;
+    return updatedProduct;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: number): void {
+    const productIndex = this.products.findIndex((p) => p.id === id);
+    if (productIndex === -1)
+      throw new NotFoundException(`Product #${id} not found`);
+
+    this.products.splice(productIndex, 1);
   }
 }
